ff606af97f9e286f2ab2b0421cad405574a2c27d
JustinBreneman <Justin.Breneman@yahoo.com>
2020-07-18 15:50:34 -0700

    updated relic class arrays

--- lib/Warframe_relics/relics.rb
+++ lib/Warframe_relics/relics.rb
@@ -1,8 +1,12 @@
 class WarframeRelics::Relics
+    
     attr_accessor :name, :url
+    
+    @@all_unsorted = []
     @@vaulted = []
     @@un-vaulted = []
-    @@all = [@@vaulted, @@un-vaulted]
+    @@all_sorted = [@@vaulted, @@un-vaulted]
+    
     def initialize(name, url)
         @name = name
         @url = url
