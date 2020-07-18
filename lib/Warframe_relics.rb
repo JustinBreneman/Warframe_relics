db3164025fcc1b782e27f7845f738e265a77cba2
JustinBreneman <Justin.Breneman@yahoo.com>
2020-07-18 15:32:56 -0700

    added requirement for relics and scrapper class

--- lib/Warframe_relics.rb
+++ lib/Warframe_relics.rb
@@ -1,5 +1,8 @@
 require_relative "./Warframe_relics/version"
 require_relative "./Warframe_relics/cli"
+require_relative "./Warframe_relics/relics"
+require_relative "./Warframe_relics/scraper"
+
 
 module WarframeRelics
   class Error < StandardError; end
