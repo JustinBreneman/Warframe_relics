d71cf64740653e03ab5f437980ea852c9152c4d0
JustinBreneman <Justin.Breneman@yahoo.com>
2020-07-23 14:35:44 -0700

    added case statements to call method

--- lib/Warframe_relics/cli.rb
+++ lib/Warframe_relics/cli.rb
@@ -6,7 +6,18 @@ class WarframeRelics::CLI
         WarframeRelics::Relics.sort_relics
         puts "I can provide a list of relics: vaulted, available, or all."
         puts "I can also provide information on a specific relic."
+        puts "Type exit at any time to leave."
         puts "What information would you like?"
         input = gets.chomp
+        if input.downcase == 'exit'
+            exit
+        elsif input.downcase == 'vaulted'
+            WarframeRelics::Relics.vaulted
+        elsif input.downcase == 'available'
+            WarframeRelics::Relics.un_vaulted
+        elsif input.downcase == 'all'
+            WarframeRelics::Relics.all_sorted
+        else
+            WarframeRelics::Relics.get_drop_table(input)
     end
 end
