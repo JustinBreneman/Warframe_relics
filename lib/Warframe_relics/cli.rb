d2bd53bb24b0ec405f6eae870ba76f898a067ad8
JustinBreneman <Justin.Breneman@yahoo.com>
2020-07-23 13:55:28 -0700

    updated CLI call method

--- lib/Warframe_relics/cli.rb
+++ lib/Warframe_relics/cli.rb
@@ -1,5 +1,12 @@
 class WarframeRelics::CLI
     def call
         puts "Welcome to the Warframe Relic scraping interface!"
+        puts "Now retrieving Relic information"
+        WarframeRelics::Relics.get_all_relics
+        WarframeRelics::Relics.sort_relics
+        puts "I can provide a list of relics: vaulted, available, or all."
+        puts "I can also provide information on a specific relic."
+        puts "What information would you like?"
+        input = gets.chomp
     end
 end
