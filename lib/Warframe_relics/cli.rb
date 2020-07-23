0ed1b6e36685a025cacb917eba4cd5d8a40b63cd
JustinBreneman <Justin.Breneman@yahoo.com>
2020-07-23 14:53:14 -0700

    tied case statements into a loop

--- lib/Warframe_relics/cli.rb
+++ lib/Warframe_relics/cli.rb
@@ -4,21 +4,24 @@ class WarframeRelics::CLI
         puts "Now retrieving Relic information"
         WarframeRelics::Relics.get_all_relics
         WarframeRelics::Relics.sort_relics
-        puts "I can provide a list of relics: vaulted, available, or all."
-        puts "I can also provide information on a specific relic."
-        puts "Type exit at any time to leave."
-        puts "What information would you like?"
-        input = gets.chomp
-        if input.downcase == 'exit'
-            exit
-        elsif input.downcase == 'vaulted'
-            WarframeRelics::Relics.vaulted
-        elsif input.downcase == 'available'
-            WarframeRelics::Relics.un_vaulted
-        elsif input.downcase == 'all'
-            WarframeRelics::Relics.all_sorted
-        else
-            WarframeRelics::Relics.get_drop_table(input)
+        input = " "
+        until input == 'exit'
+            puts "I can provide a list of relics: vaulted, available, or all."
+            puts "I can also provide information on a specific relic."
+            puts "Type exit at any time to leave."
+            puts "What information would you like?"
+            input = gets.chomp
+            if input.downcase == 'vaulted'
+                WarframeRelics::Relics.vaulted
+            elsif input.downcase == 'available'
+                WarframeRelics::Relics.un_vaulted
+            elsif input.downcase == 'all'
+                WarframeRelics::Relics.all_sorted
+            elsif input.downcase == 'exit'
+                puts "Goodbye."
+            else
+                WarframeRelics::Relics.get_drop_table(input)
+            end
         end
     end
 end
